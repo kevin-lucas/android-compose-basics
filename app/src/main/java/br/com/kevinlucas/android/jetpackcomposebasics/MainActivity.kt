18d322aa3e524742d45b0592a702b98def3ceeac
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2022-11-21 15:49:26 -0300

    Modifiers

--- app/src/main/java/br/com/kevinlucas/android/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/br/com/kevinlucas/android/jetpackcomposebasics/MainActivity.kt
@@ -1,9 +1,13 @@
 package br.com.kevinlucas.android.jetpackcomposebasics
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -20,44 +24,39 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           /**
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Green),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .width(600.dp)
+                    //.requiredWidth(600.dp)]
+                    // .padding(16.dp)
+                    // .padding(top = 50.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
             ) {
-                Text(text = "Hello")
+                // Text(text = "Hello", modifier = Modifier.offset(0.dp, 20.dp))
+                /*
+                Text(text = "Hello",
+                    modifier = Modifier
+                        .border(5.dp, Color.Yellow)
+                        .padding(5.dp)
+                        .offset(20.dp, 20.dp)
+                        .border(10.dp, Color.Gray)
+                        .padding(10.dp)
+                )
+                 */
+                Text(text = "Hello", modifier = Modifier.clickable {
+                    Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_SHORT).show()
+                })
+                // Modifier.scrollable
+                Spacer(modifier = Modifier.height(50.dp))
                 Text(text = "World")
-                Text(text = "Hello")
-            }
-            **/
-            Row(
-                modifier = Modifier
-                    //.fillMaxSize(2.5f)
-                    .width(200.dp)
-                    //.height(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Text(text = "Hello")
-                Text(text = "World")
-                Text(text = "Hello")
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Android")
 }
