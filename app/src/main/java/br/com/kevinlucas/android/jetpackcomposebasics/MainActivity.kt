136e886e69a098656f7fa7273bf8af6367ef5e4f
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2022-11-21 15:05:53 -0300

    Rows, Columns & Basic Sizing

--- app/src/main/java/br/com/kevinlucas/android/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/br/com/kevinlucas/android/jetpackcomposebasics/MainActivity.kt
@@ -3,20 +3,50 @@ package br.com.kevinlucas.android.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.kevinlucas.android.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Greeting(name = "Kevin")
+           /**
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(text = "Hello")
+                Text(text = "World")
+                Text(text = "Hello")
+            }
+            **/
+            Row(
+                modifier = Modifier
+                    //.fillMaxSize(2.5f)
+                    .width(200.dp)
+                    //.height(300.dp)
+                    .fillMaxHeight(0.7f)
+                    .background(Color.Green),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                Text(text = "Hello")
+                Text(text = "World")
+                Text(text = "Hello")
+            }
         }
     }
 }
