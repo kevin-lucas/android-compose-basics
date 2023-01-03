63ed9bfe33049d62a34887632c2816a2188505f7
Kevin Lucas <kevinlucas.tec.info@gmail.com>
2023-01-03 05:50:26 -0300

    Creating an Image Card Composable

--- app/src/main/java/br/com/kevinlucas/android/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/br/com/kevinlucas/android/jetpackcomposebasics/MainActivity.kt
@@ -1,62 +1,95 @@
 package br.com.kevinlucas.android.jetpackcomposebasics
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.kevinlucas.android.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            val painter = painterResource(id = R.drawable.kermit)
+            val description = "Kermit playing in the snow"
+            val title = "Kermit is playing in the snow"
+            Box(
                 modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .width(600.dp)
-                    //.requiredWidth(600.dp)]
-                    // .padding(16.dp)
-                    // .padding(top = 50.dp)
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
             ) {
-                // Text(text = "Hello", modifier = Modifier.offset(0.dp, 20.dp))
-                /*
-                Text(text = "Hello",
-                    modifier = Modifier
-                        .border(5.dp, Color.Yellow)
-                        .padding(5.dp)
-                        .offset(20.dp, 20.dp)
-                        .border(10.dp, Color.Gray)
-                        .padding(10.dp)
-                )
-                 */
-                Text(text = "Hello", modifier = Modifier.clickable {
-                    Toast.makeText(this@MainActivity, "Hello", Toast.LENGTH_SHORT).show()
-                })
-                // Modifier.scrollable
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "World")
+                ImageCard(painter = painter, contentDescription = description, title = title)
             }
         }
     }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop,
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ImageCardPreview() {
+    ImageCard(
+        painter = painterResource(id = R.drawable.kermit),
+        contentDescription = "Kermit",
+        title = "Kermit"
+    )
 }
